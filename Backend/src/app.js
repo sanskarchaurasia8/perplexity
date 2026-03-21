c505cd502d531c434db9844cbfe827d2cfd8d2fb
sanskarchaurasia8 <chaurasiasanskar8@gmail.com>
2026-03-21 18:39:39 +0530

    Deployed Successfully

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,16 +7,16 @@ import cors from "cors";
 
 const app = express();
 
+app.use(cors({
+    origin: "https://perplexity-eosin.vercel.app",
+    credentials: true
+}));
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
-app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:5176", "http://localhost:5177", "http://localhost:5178", "http://localhost:5179", "http://localhost:5180", "http://localhost:5181", "http://localhost:5182", "http://localhost:5183"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-}));
 
 
 // Health check
